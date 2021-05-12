920ab5f1c4fc0540e864e177527707f718a1ad41
pooneth <jtharel@yahoo.com>
2021-05-12 14:52:21 -0600

    Update scp_paramiko.py

--- scp_paramiko.py
+++ scp_paramiko.py
@@ -7,7 +7,7 @@ ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh.connect('192.168.1.251', username='jtharel', password='Fluffkin101!')
+ssh.connect('192.168.1.251', username='jtharel', password='XXXXXXX')
 
 scp = ssh.open_sftp()
 
